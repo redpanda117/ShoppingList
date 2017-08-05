27b2d21d2031a6b76aae0e4f7690e6911e09a84f
Tina Nguyen <nguyentina2005@yahoo.com>
2017-08-05 16:27:48 -0400

    file pathing

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,15 @@ CREATE TABLE burger(
     id INTEGER(11) AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(100),
     devoured BOOLEAN DEFAULT False,
-    //data TIMESTAMP,
+    date TIMESTAMP,
     PRIMARY KEY (id)
 );
+
+INSERT INTO burger (burger_name,devoured)
+VALUES ("BLT",False);
+
+INSERT INTO burger (burger_name,devoured)
+VALUES ("Cheeseburger",False);
+
+INSERT INTO burger (burger_name,devoured)
+VALUES ("Veggie Burger",True);
