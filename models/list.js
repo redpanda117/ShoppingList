c742bf9ad4368968fa6ab7b90a7e5f2a1904bc9f
Tina Nguyen <nguyentina2005@yahoo.com>
2017-08-09 09:42:01 -0400

    list.js complete
    connecting files in server.js and completed list.js

---
+++ models/list.js
@@ -0,0 +1,30 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var cat = {
+//select all items 
+  selectAll: function(cb) {
+    orm.selectAll("items", function(res) {
+      cb(res);
+    });
+  },
+    
+// The variables cols and vals are arrays.
+// add one item to database
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("items", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+
+//chane boolean status of exsiting item
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("items", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = cat;
--- server.js
+++ server.js
@@ -12,15 +12,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-
 //getting the static files here it is the images and css
-app.use(express.static(__dirname + '/public
+app.use(express.static(__dirname + "/public"));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));                       
                        
 // Set Handlebars as the default templating engine.
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");                       
                        
+// Import routes and give the server access to them.
+var routes = require("./controllers/item_controller.js");
+
+app.use("/", routes);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
